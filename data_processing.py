da858b1706bb835ab5cdc20764c663d623689d99
JakeMoraga <moragaj@hawaii.edu>
2024-10-27 16:47:28 -1000

    Here is the step to pull the data that I would like for this plot

--- data_processing.py
+++ data_processing.py
@@ -2,5 +2,8 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-#this uploads the data without the first row containing the headers
-coral_data = pd.read_csv('Datasets/coral_forecast.csv', skiprows=[1])
+#this uploads the data without the first row containing units so the data 
+#starts in the first row and also only uses the columns that I want to look
+#at which are coral cover and latitude
+main_df = pd.read_csv('coral_project/Coral_Dataset/coral_forecast.csv', skiprows= [1],
+usecols= ['coral_cover_2020', 'coral_cover_2100', 'latitude'])
