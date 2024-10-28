fcbd0287a2e5d21daaa15563e18ebb6e0f9c866a
JakeMoraga <moragaj@hawaii.edu>
2024-10-27 16:53:55 -1000

    Making sure that the info type is a float since they are long with decimal points

--- data_processing.py
+++ data_processing.py
@@ -5,5 +5,10 @@ import pandas as pd
 #this uploads the data without the first row containing units so the data 
 #starts in the first row and also only uses the columns that I want to look
 #at which are coral cover and latitude
-main_df = pd.read_csv('coral_project/Coral_Dataset/coral_forecast.csv', skiprows= [1],
+df = pd.read_csv('coral_forecast.csv', skiprows= [1],
 usecols= ['coral_cover_2020', 'coral_cover_2100', 'latitude'])
+
+#making sure that all the data is now a float type since they have decimal points
+df['latitude']= df['latitude'].astype(float)
+df['coral_cover_2020']= df['coral_cover_2020'].astype(float)
+df['coral_cover_2100']= df['coral_cover_2100'].astype(float)
