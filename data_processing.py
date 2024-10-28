00675d3c73ce68a865b56154658e9bdf089880ed
JakeMoraga <moragaj@hawaii.edu>
2024-10-27 17:15:14 -1000

    Opening that csv file wasn't working so I put it in a folder within this project folder to see if it would work

--- data_processing.py
+++ data_processing.py
@@ -5,7 +5,7 @@ import pandas as pd
 #this uploads the data without the first row containing units so the data 
 #starts in the first row and also only uses the columns that I want to look
 #at which are coral cover and latitude
-df = pd.read_csv('coral_forecast.csv', skiprows= [1],
+df = pd.read_csv('Coral_Dataset/coral_forecast.csv', skiprows= [1],
 usecols= ['coral_cover_2020', 'coral_cover_2100', 'latitude'])
 
 #making sure that all the data is now a float type since they have decimal points
