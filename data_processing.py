d84865ae8070098c46134f69a56bbdcae6a2a2df
JakeMoraga <moragaj@hawaii.edu>
2024-10-28 14:49:43 -1000

    Now we got bins so that I can put everything in a bar plot

--- data_processing.py
+++ data_processing.py
@@ -13,11 +13,22 @@ df['latitude']= df['latitude'].astype(float)
 df['coral_cover_2020']= df['coral_cover_2020'].astype(float)
 df['coral_cover_2100']= df['coral_cover_2100'].astype(float)
 
-#Now I find the difference of coral cover from the year 2020 to 2100
+#Now I find the difference of coral cover from the year 2020 to 2100 by subtracting coral cover
+#from 2100 from 2020
 df['coral_cover_difference'] = df['coral_cover_2020'] - df['coral_cover_2100']
 
-#I want to see my new column with the differences
-print(df['coral_cover_difference'])
+#I want to represent the data in a bar chart so the best way to do that is make bins
+#This arranges latitude which will be on the x-axis into bins ranging from -30 to 30 degrees
+#and the bins categorized by 5
+df['latitude_bin'] = pd.cut(df['latitude'], bins=np.arange(-30, 30, 5))
 
-#I want to see that it was added to the dataframe
+#This converts the new dataset "latitude_bin" to be in str format so that it can be used
+#in the graph by being the right data type
+df['latitude_bin'] = df['latitude_bin'].astype(str)
+
+#This groups the datapoints under coral_cover_difference into the different bins created and finds
+#the average of each bin so that I can plot it
+df_avg = df.groupby('latitude_bin')['coral_cover_difference'].mean().reset_index()
+
+#I want to see what my data looks like now
 print(df)
